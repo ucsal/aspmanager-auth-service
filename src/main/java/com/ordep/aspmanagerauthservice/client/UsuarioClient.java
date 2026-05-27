94fa3b22792252bff5df9bc5e1bfc53a95757817
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2026-05-27 00:47:01 -0300

    feat: cliente feign para chamada ao serviço de usuário

---
+++ src/main/java/com/ordep/aspmanagerauthservice/client/UsuarioClient.java
@@ -0,0 +1,12 @@
+package com.ordep.aspmanagerauthservice.client;
+
+import com.ordep.aspmanagerauthservice.dto.UsuarioAuthResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ASPMANAGER-USUARIO-SERVICE")
+public interface UsuarioClient {
+    @GetMapping("/usuarios/email/{email}")
+    UsuarioAuthResponse buscarPorEmail(@PathVariable String email);
+}
