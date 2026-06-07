32fec8c767eed1bae97ec4399a49c3ece0a9c98f
GabrielFraga <87135402+Gab1203@users.noreply.github.com>
2026-06-07 16:30:02 -0300

    fix: Add Swagger server annotation
    Add Swagger server annotation for OpenAPI documentation.

--- src/main/java/com/ordep/aspmanagerauthservice/AspmanagerAuthServiceApplication.java
+++ src/main/java/com/ordep/aspmanagerauthservice/AspmanagerAuthServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @OpenAPIDefinition(
